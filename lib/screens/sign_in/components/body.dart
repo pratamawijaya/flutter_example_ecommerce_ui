51381ab58cc9616e45aeff90cd3f496dfdbd7a72
pratamawijaya <set.mnemonix@gmail.com>
2021-01-31 09:01:32 +0700

    8 - complete sign in form 8 - complete sign in form 8 - complete sign in form 8 - complete sign in form 8 - complete sign in form 8 - complete sign in form 8 - complete sign in form 8 - complete sign in form

--- lib/screens/sign_in/components/body.dart
+++ lib/screens/sign_in/components/body.dart
@@ -1,8 +1,8 @@
 import 'package:ecommerce_ui/components/social_media_button.dart';
+import 'package:ecommerce_ui/constants.dart';
 import 'package:ecommerce_ui/screens/sign_in/components/sign_in_form.dart';
 import 'package:ecommerce_ui/size_config.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -14,42 +14,70 @@ class Body extends StatelessWidget {
           padding: EdgeInsets.symmetric(
             horizontal: getProportionateScreenWidth(16.0),
           ),
-          child: Column(
-            children: [
-              Text(
-                "Welcome",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: getProportionateScreenWidth(28),
-                  fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(height: SizeConfig.screenHeight * 0.04),
+                Text(
+                  "Welcome",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: getProportionateScreenWidth(28),
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Text(
-                "Sign in with your email and password \nor continue with social media account",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: getProportionateScreenWidth(12),
+                SizedBox(
+                  height: getProportionateScreenHeight(10),
                 ),
-              ),
-              SignInForm(),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SocialMediaButton(
-                    icon: "assets/icons/facebook-2.svg",
-                    press: () {},
+                Text(
+                  "Sign in with your email and password \nor continue with social media account",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: getProportionateScreenWidth(12),
                   ),
-                  SocialMediaButton(
-                    icon: "assets/icons/google-icon.svg",
-                    press: () {},
-                  ),
-                  SocialMediaButton(
-                    icon: "assets/icons/twitter.svg",
-                    press: () {},
-                  ),
-                ],
-              )
-            ],
+                ),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                SignInForm(),
+                SizedBox(height: SizeConfig.screenHeight * 0.08),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SocialMediaButton(
+                      icon: "assets/icons/facebook-2.svg",
+                      press: () {},
+                    ),
+                    SocialMediaButton(
+                      icon: "assets/icons/google-icon.svg",
+                      press: () {},
+                    ),
+                    SocialMediaButton(
+                      icon: "assets/icons/twitter.svg",
+                      press: () {},
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(20),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account ? ",
+                      style: TextStyle(
+                        fontSize: getProportionateScreenWidth(16),
+                      ),
+                    ),
+                    Text(
+                      "Sign Up",
+                      style: TextStyle(
+                          fontSize: getProportionateScreenWidth(16),
+                          color: kPrimaryColor),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
